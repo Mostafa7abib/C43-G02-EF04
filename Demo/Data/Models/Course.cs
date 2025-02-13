06b3a73de512c071a8c02d6415ad866004c8bd5e
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-02-13 13:06:31 +0200

    Video 01 - Data Seeding

---
+++ Demo/Data/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Data.Models
+{
+    internal class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public ICollection<StudentsCourses> StudentCourses { get; set; } = new HashSet<StudentsCourses>();
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Demo
+﻿using Demo.Data;
+using Demo.Data.DataSeeding;
+using Demo.Data.Models;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Session04
+            #region DataSeeding 
+            using CompanyDbContext dbContext = new CompanyDbContext();
+            DbContextSeeding.Seed(dbContext);
+            #endregion
+            #endregion
         }
     }
 }
